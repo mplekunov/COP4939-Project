edc2d4465fc07e19aed67e8814a451cf23e625ae
Mikhail Plekunov <mplekunov@gmail.com>
2023-12-22 18:49:13 -0500

    Update UI logic

--- COP4939-Project/View/StatisticsView.swift
+++ COP4939-Project/View/StatisticsView.swift
@@ -12,168 +12,170 @@ struct StatisticsView: View {
     @EnvironmentObject var dataReceiverViewModel: DataReceiverViewModel
     
     var body: some View {
+        if let session = dataReceiverViewModel.session {
         List {
-            Section("Data Points Received") {
-                Text("\(dataReceiverViewModel.session.data.count)")
-            }.listRowBackground(Color.secondary)
-            
-            Section("Location latitude") {
-                VStack {
-                    LineChartView(
-                        data: dataReceiverViewModel.session.data.map {
-                            ChartData(
-                                date: Date(timeIntervalSince1970: $0.timeStamp),
-                                data: $0.location.coordinate.latitude
-                            )
-                        }
-                    ).padding()
+                Section("Data Points Received") {
+                    Text("\(session.data.count)")
+                }.listRowBackground(Color.secondary)
+                
+                Section("Location latitude") {
+                    VStack {
+                        LineChartView(
+                            data: session.data.map {
+                                ChartData(
+                                    date: Date(timeIntervalSince1970: $0.timeStamp),
+                                    data: $0.location.coordinate.latitude
+                                )
+                            }
+                        ).padding()
+                    }
                 }
+                .listRowBackground(Color.secondary)
+                
+                Section("Location longitude") {
+                    VStack {
+                        LineChartView(
+                            data: session.data.map {
+                                ChartData(
+                                    date: Date(timeIntervalSince1970: $0.timeStamp),
+                                    data: $0.location.coordinate.longitude
+                                )
+                            }
+                        ).padding()
+                    }
+                }
+                .listRowBackground(Color.secondary)
+                
+                Section("Attitude Pitch") {
+                    VStack {
+                        LineChartView(
+                            data: session.data.map {
+                                ChartData(
+                                    date: Date(timeIntervalSince1970: $0.timeStamp),
+                                    data: $0.motion.attitude.pitch
+                                )
+                            }
+                        ).padding()
+                    }
+                }.listRowBackground(Color.secondary)
+                
+                Section("Attitude Yaw") {
+                    VStack {
+                        LineChartView(
+                            data: session.data.map {
+                                ChartData(
+                                    date: Date(timeIntervalSince1970: $0.timeStamp),
+                                    data: $0.motion.attitude.yaw
+                                )
+                            }
+                        ).padding()
+                    }
+                }.listRowBackground(Color.secondary)
+                
+                Section("Attitude Roll") {
+                    VStack {
+                        LineChartView(
+                            data: session.data.map {
+                                ChartData(
+                                    date: Date(timeIntervalSince1970: $0.timeStamp),
+                                    data: $0.motion.attitude.roll
+                                )
+                            }
+                        ).padding()
+                    }
+                }
+                .listRowBackground(Color.secondary)
+                
+                Section("G Force X") {
+                    VStack {
+                        LineChartView(
+                            data: session.data.map {
+                                ChartData(
+                                    date: Date(timeIntervalSince1970: $0.timeStamp),
+                                    data: $0.motion.gForce.x
+                                )
+                            }
+                        ).padding()
+                    }
+                }.listRowBackground(Color.secondary)
+                
+                Section("G Force Y") {
+                    VStack {
+                        LineChartView(
+                            data: session.data.map {
+                                ChartData(
+                                    date: Date(timeIntervalSince1970: $0.timeStamp),
+                                    data: $0.motion.gForce.y
+                                )
+                            }
+                        ).padding()
+                    }
+                }.listRowBackground(Color.secondary)
+                
+                Section("G Force z") {
+                    VStack {
+                        LineChartView(
+                            data: session.data.map {
+                                ChartData(
+                                    date: Date(timeIntervalSince1970: $0.timeStamp),
+                                    data: $0.motion.gForce.z
+                                )
+                            }
+                        ).padding()
+                    }
+                }
+                .listRowBackground(Color.secondary)
+                
+                Section("Acceleration X") {
+                    VStack {
+                        LineChartView(
+                            data: session.data.map {
+                                ChartData(
+                                    date: Date(timeIntervalSince1970: $0.timeStamp),
+                                    data: $0.motion.acceleration.x
+                                )
+                            }
+                        )
+                        .padding()
+                    }
+                }
+                .listRowBackground(Color.secondary)
+                
+                Section("Acceleration Y") {
+                    VStack {
+                        LineChartView(
+                            data: session.data.map {
+                                ChartData(
+                                    date: Date(timeIntervalSince1970: $0.timeStamp),
+                                    data: $0.motion.acceleration.y
+                                )
+                            }
+                        )
+                        .padding()
+                    }
+                }
+                .listRowBackground(Color.secondary)
+                
+                Section("Acceleration Z") {
+                    VStack {
+                        LineChartView(
+                            data: session.data.map {
+                                ChartData(
+                                    date: Date(timeIntervalSince1970: $0.timeStamp),
+                                    data: $0.motion.acceleration.z
+                                )
+                            }
+                        )
+                        .padding()
+                    }
+                }
+                .listRowBackground(Color.secondary)
             }
-            .listRowBackground(Color.secondary)
-            
-            Section("Location longitude") {
-                VStack {
-                    LineChartView(
-                        data: dataReceiverViewModel.session.data.map {
-                            ChartData(
-                                date: Date(timeIntervalSince1970: $0.timeStamp),
-                                data: $0.location.coordinate.longitude
-                            )
-                        }
-                    ).padding()
-                }
-            }
-            .listRowBackground(Color.secondary)
-            
-            Section("Attitude Pitch") {
-                VStack {
-                    LineChartView(
-                        data: dataReceiverViewModel.session.data.map {
-                            ChartData(
-                                date: Date(timeIntervalSince1970: $0.timeStamp),
-                                data: $0.motion.attitude.pitch
-                            )
-                        }
-                    ).padding()
-                }
-            }.listRowBackground(Color.secondary)
-            
-            Section("Attitude Yaw") {
-                VStack {
-                    LineChartView(
-                        data: dataReceiverViewModel.session.data.map {
-                            ChartData(
-                                date: Date(timeIntervalSince1970: $0.timeStamp),
-                                data: $0.motion.attitude.yaw
-                            )
-                        }
-                    ).padding()
-                }
-            }.listRowBackground(Color.secondary)
-            
-            Section("Attitude Roll") {
-                VStack {
-                    LineChartView(
-                        data: dataReceiverViewModel.session.data.map {
-                            ChartData(
-                                date: Date(timeIntervalSince1970: $0.timeStamp),
-                                data: $0.motion.attitude.roll
-                            )
-                        }
-                    ).padding()
-                }
-            }
-            .listRowBackground(Color.secondary)
-            
-            Section("G Force X") {
-                VStack {
-                    LineChartView(
-                        data: dataReceiverViewModel.session.data.map {
-                            ChartData(
-                                date: Date(timeIntervalSince1970: $0.timeStamp),
-                                data: $0.motion.gForce.x
-                            )
-                        }
-                    ).padding()
-                }
-            }.listRowBackground(Color.secondary)
-            
-            Section("G Force Y") {
-                VStack {
-                    LineChartView(
-                        data: dataReceiverViewModel.session.data.map {
-                            ChartData(
-                                date: Date(timeIntervalSince1970: $0.timeStamp),
-                                data: $0.motion.gForce.y
-                            )
-                        }
-                    ).padding()
-                }
-            }.listRowBackground(Color.secondary)
-            
-            Section("G Force z") {
-                VStack {
-                    LineChartView(
-                        data: dataReceiverViewModel.session.data.map {
-                            ChartData(
-                                date: Date(timeIntervalSince1970: $0.timeStamp),
-                                data: $0.motion.gForce.z
-                            )
-                        }
-                    ).padding()
-                }
-            }
-            .listRowBackground(Color.secondary)
-            
-            Section("Acceleration X") {
-                VStack {
-                    LineChartView(
-                        data: dataReceiverViewModel.session.data.map {
-                            ChartData(
-                                date: Date(timeIntervalSince1970: $0.timeStamp),
-                                data: $0.motion.acceleration.x
-                            )
-                        }
-                    )
-                    .padding()
-                }
-            }
-            .listRowBackground(Color.secondary)
-            
-            Section("Acceleration Y") {
-                VStack {
-                    LineChartView(
-                        data: dataReceiverViewModel.session.data.map {
-                            ChartData(
-                                date: Date(timeIntervalSince1970: $0.timeStamp),
-                                data: $0.motion.acceleration.y
-                            )
-                        }
-                    )
-                    .padding()
-                }
-            }
-            .listRowBackground(Color.secondary)
-            
-            Section("Acceleration Z") {
-                VStack {
-                    LineChartView(
-                        data: dataReceiverViewModel.session.data.map {
-                            ChartData(
-                                date: Date(timeIntervalSince1970: $0.timeStamp),
-                                data: $0.motion.acceleration.z
-                            )
-                        }
-                    )
-                    .padding()
-                }
-            }
-            .listRowBackground(Color.secondary)
+            .padding()
+            .background(.black)
+            .foregroundStyle(.orange)
+            .scrollContentBackground(.hidden)
         }
-        .padding()
-        .background(.black)
-        .foregroundStyle(.orange)
-        .scrollContentBackground(.hidden)
     }
 }
 
